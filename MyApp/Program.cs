d34c8cc58adcc8071e98b1e3e558ba083592ac58
emybarr <eybarreiro.2a@gmail.com>
2022-05-21 14:18:39 -0300

    calculadorav2

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,12 +1,64 @@
-﻿int numero, ninverso = 0;
+﻿int continuar = 1, operador;
 
-Console.WriteLine("Ingrese el número a invertir:");
-numero= Convert.ToInt32(Console.ReadLine());
+double n1= 0 ,n2= 0, resultado= 0;
 
-while(numero > 0 && numero != 0){
-    ninverso = ninverso * 10 + (numero%10);
-    numero= numero/10;
-}
-Console.WriteLine($"El numero invertido es {ninverso}");
+do{
+    Console.WriteLine("Elegir la Opecion a Realizar ");
+    do{
+        Console.WriteLine(" 5-Valor absoluto\n 6-Cuadrado \n 7-Raíz cuadrada\n 8-Seno \n 9-Coseno\n 10-Parte Entera\n");
+        operador=Convert.ToInt32(Console.ReadLine());
+    }while(operador < 5 || operador > 10);
 
+    Console.WriteLine("Ingreser un numero : ");
+    n1 = Convert.ToDouble(Console.ReadLine());
+
+    if(operador != 0 ){
+        switch(operador){
+            case 5 : 
+            resultado = Math.Abs(n1);
+            Console.WriteLine($"El valor Absoluto {n1} es {resultado}\n");
+            break;
+            case 6 : 
+            resultado = Math.Pow(n1,2) ;
+            Console.WriteLine($"Potencia de {n1} es {resultado}\n");
+            break;
+            case 7 : 
+            resultado = Math.Sqrt(n1) ;
+            Console.WriteLine($" La raiz Cuadrada {n1} es  {resultado}\n");
+            break;
+            case 8 : 
+            resultado = Math.Sin(n1 * Math.PI /180) ;
+            Console.WriteLine($" El seno {n1} es  {resultado}\n");
+            break;
+            case 9 : 
+            resultado = Math.Cos(n1 * Math.PI /180) ;
+            Console.WriteLine($" El coseno {n1} es  {resultado}\n");
+            break;
+            case 10 : 
+            resultado = Math.Truncate(n1) ;
+            Console.WriteLine($" La parte entera {n1} es  {resultado} \n");
+            break;
+    
+        }   
+    }
+  
+    Console.WriteLine("\nIngreser un numero : ");
+    n1 = Convert.ToDouble(Console.ReadLine());
+    
+    Console.WriteLine("Ingreser un numero : ");
+    n2 = Convert.ToDouble(Console.ReadLine());
+ 
+    Console.WriteLine($"Numero Maximo : {Math.Max(n1,n2)}");
+    Console.WriteLine($"Numero Minimo : {Math.Min(n1,n2)}");
+
+
+
+
+    do{ 
+        Console.WriteLine("Seguir usando la calculadoa 1-SI ");
+        continuar=Convert.ToInt32(Console.ReadLine());
+
+    }while(continuar == 0);
+
+}while(continuar == 1 );
 

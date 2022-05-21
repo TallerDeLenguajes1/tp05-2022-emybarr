c8b0c5f60756b8d9dea0663989cebb62b9823723
emybarr <eybarreiro.2a@gmail.com>
2022-05-21 10:53:38 -0300

    punto1

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola, Mundo!");
+﻿int numero, ninverso = 0;
+
+Console.WriteLine("Ingrese el número a invertir:");
+numero= Convert.ToInt32(Console.ReadLine());
+
+while(numero > 0 && numero != 0){
+    ninverso = ninverso * 10 + (numero%10);
+    numero= numero/10;
+}
+Console.WriteLine($"El numero invertido es {ninverso}");
+
+

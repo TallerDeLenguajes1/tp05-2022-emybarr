8e6ccf066e6c9dd0df2eaacd7ce449808c0bbd94
emybarr <eybarreiro.2a@gmail.com>
2022-05-24 00:57:19 -0300

    punto4

---
+++ Punto4/Program.cs
@@ -0,0 +1,81 @@
+﻿Console.WriteLine("-----------------Punto 4 ------------- ");
+
+Console.WriteLine("Ingrese un texto\n ");
+
+string cadena = Console.ReadLine();
+
+string parteCadena = cadena.Substring(0,6);
+
+Console.WriteLine($"Parte de la cadema :  {parteCadena} \n");
+
+Console.WriteLine($"Parte de la cadema :  {cadena.Length} \n");
+
+
+Console.WriteLine("Concatenar (Unir) dos cadenas distintas ");
+string cadena1= "Hola ";
+Console.WriteLine($"Cadena 1:  {cadena1} \n");
+string cadena2= "a todos ";
+Console.WriteLine($"Cadena 1:  {cadena2} \n");
+string cad= string.Concat(cadena1,cadena2);
+Console.WriteLine($"Concatenacion de cadena :  {cad} \n");
+
+
+Console.WriteLine("Extraer una subcadena ");
+string subcadena = cad.Substring(6);
+Console.WriteLine($"Substraer cadena  :  {subcadena} \n");
+
+Console.WriteLine("Recorrer la cadena de texto con un ciclo Foreach ");
+foreach(char letra in cad){
+    Console.WriteLine(letra);
+}
+
+Console.WriteLine("Buscar la ocurrencia de una palabra determinada en la cadena ingresada ");
+string cadena3= Console.ReadLine();
+string palabra ="la";
+Console.WriteLine($"La cantidad de veces que aparece una palabra es : {cadena3.Contains(palabra)}");
+
+
+Console.WriteLine("Convierta la cadena a Mayúsculas y luego minúsculas");
+string cadena4 = "rinoceROnTe";
+Console.WriteLine($"Mayusculas {cadena4.ToUpper()}");
+Console.WriteLine($"Minusculas {cadena4.ToLower()}");
+
+Console.WriteLine("Ingrese una nueva cadena y luego compare los valores de ambas.");
+string cadena5 = "Licenciatura en informática";
+Console.WriteLine($"Comparar {String.Equals(cad,cadena5)}");
+
+Console.WriteLine("Ingrese una cadena separada por caracteres que usted determine");
+string cadena6= "Returns-a-value-indicating-whether";
+string[] separar = cadena6.Split("-");
+foreach( string frase in separar){
+    Console.WriteLine(frase);
+}
+
+
+Console.WriteLine("ingrese una ecuación simple como cadena de caracteres y que el sistema lo resuelva");
+string ecuacion = Console.ReadLine();
+
+double resultado = 0;
+switch(ecuacion){
+    case var s when ecuacion.Contains("+"):
+    resultado = Convert.ToDouble(ecuacion.Split("+")[0]) + Convert.ToDouble(ecuacion.Split("+")[1]);
+    Console.WriteLine($"El resultado es {resultado}");
+    break;
+    case var s when ecuacion.Contains("-"):
+    resultado = Convert.ToDouble(ecuacion.Split("-")[0]) - Convert.ToDouble(ecuacion.Split("-")[1]);
+    Console.WriteLine($"El resultado es {resultado}");
+    break;
+    case var s when ecuacion.Contains("*"):
+    resultado = Convert.ToDouble(ecuacion.Split("*")[0]) * Convert.ToDouble(ecuacion.Split("*")[1]);
+    Console.WriteLine($"El resultado es {resultado}");
+    break;
+    case var s when ecuacion.Contains("/"):
+    if(Convert.ToDouble(ecuacion.Split("/")[1]) == 0){
+        Console.WriteLine("ERROR");
+    }else{
+    resultado = Convert.ToDouble(ecuacion.Split("/")[0]) / Convert.ToDouble(ecuacion.Split("/")[1]);
+    Console.WriteLine($"El resultado es {resultado}");
+    }
+    break;
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # tp05-2022-emybarr
+
+# ¿String es una tipo por valor o un tipo por referencia?
+
+Un tipo de datos es un tipo de valor si contiene los datos dentro de su propia asignación de memoria
+Un tipo de referencia almacena una referencia a sus datos. String es un tipo de referencia 
+
+#  ¿Qué secuencias de escape tiene el tipo string?
+| Secuencia de escape |	Representa |
+| ---------- | ----------|
+| \a |	Campana (alerta) |
+| \b | Retroceso |
+| f  |	Avance de página |
+| \n | Nueva línea |
+| \r |Retorno de carro |
+| \t | Tabulación horizontal |
+| \v |Tabulación vertical |
+| \' |Comilla simple |
+| \" |Comillas dobles |
+| \\ |Barra diagonal inversa |
+| \? |Signo de interrogación literal |
+|\ooo|	Carácter ASCII en notación octal | 
+|\xhh|	Carácter ASCII en notación hexadecimal |
+|\xhhhh	|Carácter Unicode en notación hexadecimal |
+
+# ¿Qué sucede cuando utiliza el carácter @ y $ antes de una cadena de texto?
+ @, el carácter de identificador textual.
+ $, el carácter de cadena interpolada
+
 tp05-2022-emybarr created by GitHub Classroom
